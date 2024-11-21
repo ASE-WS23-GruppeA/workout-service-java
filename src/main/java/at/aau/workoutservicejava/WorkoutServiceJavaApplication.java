e6baf3a22007197fe2d907f78f99f2476a0c06f4
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-11-21 13:26:15 +0100

    Initialize project

---
+++ src/main/java/at/aau/workoutservicejava/WorkoutServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package at.aau.workoutservicejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutServiceJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkoutServiceJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/at/aau/workoutservicejava/WorkoutServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package at.aau.workoutservicejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutServiceJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
